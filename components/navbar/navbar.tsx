619af4fd803dc3ca1d00e876c2b5fc42ae16d470
felana-niaina <nirina.felananiaina@gmail.com>
2025-01-28 14:01:23 +0300

    delete useeffect

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FaBars, FaMoon, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
